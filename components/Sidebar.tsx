29ad66809df919f92a1472237e0ce2f89f793c5c
victoryhwan7 <victoryhwan7@gmail.com>
2022-05-06 15:33:54 +0900

    사이드메뉴 토글적용

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,14 +7,18 @@ import fs from 'fs';
 import path from 'path';
 
 type Props = {
-    children?: SideProps
+    children?: PostItem
 }
 
-type SideProps = {
-    title: string;
-    gubun: string;
-    path: string;
-}
+// type SideProps = {
+//     title: string;
+//     gubun: string;
+//     path: string;
+// }
+
+// type SideProps2 = {
+//     children?: PostItem
+// }
 
 type PostItem = {
     name?: string
@@ -30,35 +34,14 @@ const Sidebar = ({postList}:PostListProps)=>{
 
     const rendering = () => {
         const rs = []
+        
+        // console.log(`rendering`)
         for(let i=0; i<postList.length; i++){
-            let obj = {
-                title : `${postList[i].name}(${postList[i].postCnt})`,
-                gubun : "category",
-                path : ""
-            }
-            rs.push(<SideBar_li key={i}>{obj}</SideBar_li>);
-            for(let j=0; j<postList[i].posts.length; j++){
-                let subTitle = postList[i].posts[j]+ ""
-                let subObj = {
-                    title : subTitle.substring(0,subTitle.length-4),
-                    gubun : "post",
-                    path : postList[i].name
-                }
-                rs.push(<SideBar_li key={`post${postList[i].name}_${j}`}>{subObj}</SideBar_li>);
-            }
+            rs.push(<SideBar_li key={i}>{postList[i]}</SideBar_li>);
         }
         return rs
     }
-    // fixed z-40 inset-0 flex-none h-full bg-black bg-opacity-25 w-full lg:bg-white lg:static lg:h-auto lg:overflow-y-visible lg:pt-0 lg:w-60 xl:w-72 lg:block hidden
 
-    // fixed z-40 inset-0 flex-none h-full bg-black bg-opacity-25 w-full lg:bg-white lg:static lg:h-auto lg:overflow-y-visible lg:pt-0 lg:w-60 xl:w-72 lg:block hidden
-    // fixed z-40 inset-0 flex-none bg-gray-200 w-full lg:static lg:h-auto lg:overflow-y-visible lg:pt-0 lg:w-60 xl:w-72 lg:block hidden
-
-    //h-full overflow-hidden scrolling-touch lg:h-auto lg:block lg:relative lg:bg-transparent lg:top-18 mr-24 lg:mr-0
-    //h-full overflow-y-auto scrolling-touch lg:h-auto lg:block lg:relative lg:sticky lg:bg-transparent overflow-hidden lg:top-18 bg-white mr-24 lg:mr-0
-
-    //px-1 pt-6 overflow-y-auto font-medium text-base sm:px-3 xl:px-5 lg:text-sm pb-10 lg:pt-10 lg:pb-14 sticky?lg:h-(screen-18)
-    //px-1 pt-6 overflow-y-auto font-medium text-base sm:px-3 xl:px-5 lg:text-sm pb-10 lg:pt-10 lg:pb-14 sticky?lg:h-(screen-18)
     return (
         
         <>            
@@ -68,25 +51,8 @@ const Sidebar = ({postList}:PostListProps)=>{
                         <ul>
                             <li className="mt-8">
                                 <h5 className="px-3 mb-3 lg:mb-3 uppercase tracking-wide font-semibold text-sm lg:text-xs text-gray-900">contents index list</h5>
-                                <ul>
-                                    {rendering()}
-                                </ul>
-
                                 <ol>
-                                    <li className="toggle">
-                                        <details>
-                                            <summary>CSS first steps</summary>
-                                            <ol>
-                                                <li><a href="/en-US/docs/Learn/CSS/First_steps">CSS first steps overview</a></li>
-                                                <li><a href="/en-US/docs/Learn/CSS/First_steps/What_is_CSS">What is CSS?</a></li>
-                                                <li><a href="/en-US/docs/Learn/CSS/First_steps/Getting_started">Getting started with CSS</a></li>
-                                                <li><a href="/en-US/docs/Learn/CSS/First_steps/How_CSS_is_structured">How CSS is structured</a></li>
-                                                <li><a href="/en-US/docs/Learn/CSS/First_steps/How_CSS_works">How CSS works</a></li>
-                                                <li><a href="/en-US/docs/Learn/CSS/First_steps/Styling_a_biography_page">Using your new knowledge</a></li>
-                                            </ol>
-                                        </details>
-                                    </li>
-
+                                    {rendering()}
                                 </ol>
                             </li>
                         </ul>
@@ -99,30 +65,33 @@ const Sidebar = ({postList}:PostListProps)=>{
 
 function SideBar_li ({children}: Props){
 
-    let title = children.title
-
-    if(children.gubun == 'category'){
-        return (
-            <li>
-                <a className="px-3 py-2 transition-colors duration-200 relative block text-gray-800 rounded-md">
-                    {/* <span className="rounded-md absolute inset-0 bg-cyan-50 opacity-0">zz</span> */}
-                    <span className="relative">{children.title}</span>
-                </a>
-            </li>
-        )
-    }else{
-        return (
-            <li>
-                <Link href={`/post/${children.path}/${children.title}`}>
-                    <a className="px-7 py-2 transition-colors duration-200 relative block hover:text-gray-900 hover:bg-yellow-200 text-gray-500 rounded-md">
-                        {/* <span className="rounded-md absolute inset-0 bg-cyan-50 opacity-0">zz</span> */}
-                        <span className="relative">{children.title}</span>
-                    </a>
-                </Link>
-            </li>
-        )
-    }
+    // console.log(`SideBar_li_v2`, children)
+    let summaryTitle = `${children.name}(${children.postCnt})`
+    return (
+        <li className="toggle">
+            <details>
+                <summary>{summaryTitle}</summary>
+                <ol>
+                    {
+                        children.posts.map((item :String, idx)=>{
+                            let title = item.substring(0,item.length-4)
+                            return (
+                                <li key={idx}>
+                                    <Link href={`/post/${children.name}/${title}`}>
+                                        <a className='side-menu-item'>
+                                            <span className="relative">{title}</span>
+                                        </a>
+                                    </Link>
+                                </li>        
+                            )
+                        })
+                    }
+                </ol>
+            </details>
+        </li>
+    )
 }
+
 export default Sidebar
 // 
   
