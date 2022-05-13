af06dd2bb5ee137381814156117e8f551c51fdd0
victoryhwan7 <victoryhwan7@gmail.com>
2022-05-13 18:48:15 +0900

    side item test

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import {getAllPostDir} from '../utils/mdxUtils'
 
 import fs from 'fs';
 import path from 'path';
+import Button from '@mui/material/Button';
 
 type Props = {
     children?: PostItem
@@ -43,10 +44,11 @@ const Sidebar = ({postList}:PostListProps)=>{
     }
 
     return (
-        
+        //hidden lg:block fixed z-20 inset-0 top-[3.8125rem] left-[max(0px,calc(50%-45rem))] right-auto w-[19.5rem] pb-10 px-8 overflow-y-auto
+        //absolute z-40 inset-0 flex-none h-full bg-black bg-opacity-25 w-full lg:bg-gray-100 lg:static lg:h-auto lg:overflow-y-visible lg:pt-0 lg:w-60 xl:w-72 lg:block hidden
         <>            
-            <div id="sidebar" className="absolute z-40 inset-0 flex-none h-full bg-black bg-opacity-25 w-full lg:bg-gray-100 lg:static lg:h-auto lg:overflow-y-visible lg:pt-0 lg:w-60 xl:w-72 lg:block hidden">
-                <div id="navWrapper" className="h-full overflow-y-auto scrolling-touch lg:h-auto lg:block lg:sticky lg:bg-transparent overflow-hidden lg:top-18 bg-white mr-24 lg:mr-0">
+            <div id="sidebar" className="z-40 inset-0 flex-none h-full bg-black bg-opacity-25 w-full lg:bg-gray-100 lg:static lg:h-auto  lg:pt-0 lg:w-60 xl:w-72 lg:block hidden">
+                <div id="navWrapper" className="h-full overflow-y-auto scrolling-touch lg:h-auto lg:block lg:bg-transparent lg:top-18 bg-white mr-24 lg:mr-0 ">
                     <div id="nav" className="px-1 pt-6 overflow-y-auto font-medium text-base sm:px-3 xl:px-5 lg:text-sm pb-10 lg:pt-10 lg:pb-14 sticky?lg:h-(screen-18)">
                         <ul>
                             <li className="mt-8">
@@ -54,9 +56,21 @@ const Sidebar = ({postList}:PostListProps)=>{
                                 <ol>
                                     {rendering()}
                                 </ol>
+                                <ol>
+                                    {rendering()}
+                                </ol>
+                                <ol>
+                                    {rendering()}
+                                </ol>
+                                <ol>
+                                    {rendering()}
+                                </ol>
+
                             </li>
                         </ul>
+                        <Button variant="contained" className='bg-black hover:bg-gray-500' >Text</Button>
                     </div>
+
                 </div>
             </div>
         </>
